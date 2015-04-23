a2a5aeb3132896a44e37eb624427afb4fedf5c0b
Nyphon Development <Nyphon.Development@gmail.com>
2015-04-23 14:32:23 +0200

    More Changes for Better Programming

--- app/src/main/java/de/nyphon/materialchat/Modules/AppService.java
+++ app/src/main/java/de/nyphon/materialchat/Modules/AppService.java
@@ -1,15 +1,54 @@
 package de.nyphon.materialchat.Modules;
 
-import android.app.Service;
+import android.app.IntentService;
 import android.content.Intent;
-import android.os.IBinder;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft_10;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Created by Ralph Schuler on 21.04.2015.
  */
-public class AppService extends Service {
+public class AppService extends IntentService {
+
+    private WebSocketClient mWebSocketClient;
+
+    public AppService(String name) {
+        super(name);
+    }
+
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    protected void onHandleIntent(Intent intent) {
+        try {
+            mWebSocketClient = new WebSocketClient(new URI("ws://52.17.85.63:8080"), new Draft_10()) {
+                @Override
+                public void onOpen(ServerHandshake serverHandshake) {
+
+                }
+
+                @Override
+                public void onMessage(String s) {
+
+                }
+
+                @Override
+                public void onClose(int i, String s, boolean b) {
+
+                }
+
+                @Override
+                public void onError(Exception e) {
+
+                }
+            };
+
+            mWebSocketClient.run();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/de/nyphon/materialchat/Splash.java
+++ app/src/main/java/de/nyphon/materialchat/Splash.java
@@ -14,7 +14,5 @@ public class Splash extends Activity {
 
     @Override
     public void onBackPressed() {
-
-        super.onBackPressed();
     }
 }
